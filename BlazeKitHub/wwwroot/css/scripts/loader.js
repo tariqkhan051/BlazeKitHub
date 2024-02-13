fe480b00fbfa4f82736e08229fd74481ddc8247a
Tariq Khan <tariqkhan051@outlook.com>
2024-02-13 22:08:10 +0400

    - credentials manager: layout and content updated, password field handling updated - deployments: closing modal error fixed - home: content updated - links: renamed page - online tools: moved performance calculator to separate page - performance calculator: layout updated for responsiveness - sumo logs: layout updated - nav menu: version support added - readme: content updated

--- BlazeKitHub/wwwroot/css/scripts/loader.js
+++
@@ -1,8 +0,0 @@
-﻿function showLoader() {
-    const box = document.getElementById('cover-spin');
-    box.style.display = 'block';
-}
-function hideLoader() {
-    const box = document.getElementById('cover-spin');
-    box.style.display = 'none';
-}
